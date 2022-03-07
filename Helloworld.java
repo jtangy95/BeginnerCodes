9bda9448fe763fbcd46c0b0de19fe73368cd3f7a
jtangy95 <jtangy95@gmail.com>
2022-03-07 17:14:42 +0900

    java intro with w3schools

---
+++ Helloworld.java
@@ -0,0 +1,24 @@
+// HelloWorld.java
+public class Helloworld{
+    public static void main(String[] args){
+        // Prints "Hello, World" to the terminal
+        System.out.println("Hello, World!") ;
+    }
+}
+
+// class
+
+/* Every line of code that runs in Java must be inside a class. 
+In our example, we named the class `Helloworld`. 
+A class should always start with an uppercase first letter. */
+
+
+// file name
+
+/* The name of the java file must match the class name.
+When saving the file, save it using the class name and add ".java" to the end of the filename. */
+
+// main
+
+/* The main() method is required and you will see it in every Java program 
+Any code inside the main() method will be executed. */
